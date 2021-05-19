fb14a0f1eef7f77bfd87e3fb641ed4636364c02c
horhoj <horhoj@yandex.ru>
2021-05-19 11:47:23 +0500

    add update / new feature to category

--- src/app/Http/Controllers/API/CategoryController.php
+++ src/app/Http/Controllers/API/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
 use App\repositories\CategoryRepository;
 use App\Types\APIIndexRequestParams;
 use Illuminate\Http\Request;
@@ -20,11 +21,33 @@ class CategoryController extends Controller
         $this->categoryRepository = $categoryRepository;
     }
 
-    public function index(Request $request)
+    public function index(Request $request): array
     {
         $APIIndexRequestParams = new APIIndexRequestParams($request);
 
         return $this->categoryRepository->getList($APIIndexRequestParams);
+    }
+
+    public function show(Request $request): array
+    {
+        $id = $request[self::CATEGORY_ID] ?? 0;
+
+        return $this->categoryRepository->get($id);
+    }
+
+    public function update(CategoryRequest $request): array
+    {
+        $id = $request[self::CATEGORY_ID] ?? 0;
+        $data = $request->all();
+
+        return $this->categoryRepository->update($id, $data);
+    }
+
+    public function store(CategoryRequest $request): array
+    {
+        $data = $request->all();
+
+        return $this->categoryRepository->store($data);
     }
 
     public function destroy(Request $request): array
